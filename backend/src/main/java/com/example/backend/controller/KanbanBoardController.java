33532f5f9b17e41d08c9ef221646fe7ec5e24c1c
martinkascak <m.kascak93@gmail.com>
2022-11-11 12:20:25 +0100

    add controller

--- backend/src/main/java/com/example/backend/controller/KanbanBoardController.java
+++ backend/src/main/java/com/example/backend/controller/KanbanBoardController.java
@@ -1,4 +1,26 @@
 package com.example.backend.controller;
 
+
+import com.example.backend.model.Todo;
+import com.example.backend.service.KanbanService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class KanbanBoardController {
+
+    private KanbanService kanbanService;
+
+    public KanbanBoardController(KanbanService kanbanService) {
+        this.kanbanService = kanbanService;
+    }
+
+    @GetMapping("/todo")
+    public List<Todo> getAllTodos(){
+        return kanbanService.getAllTodos();
+    }
 }
